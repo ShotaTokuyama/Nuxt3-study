02ceae97e082d681211281817e7bc1892b9b492b
tokuyama_shota <tokush1031@gmail.com>
2023-08-18 16:11:24 +0900

    main page create

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,16 @@
 <template>
-  <ul class="flex">
-    <li class="mr-6 text-blue-500 hover:text-blue-800">
+  <ul class="p-4 flex justify-center gap-x-4 md:gap-x-8">
+    <li class="text-blue-500 hover:text-blue-800">
       <NuxtLink to="/">Home</NuxtLink>
     </li>
-    <li class="mr-6 text-blue-500 hover:text-blue-800">
+    <li class="text-blue-500 hover:text-blue-800">
       <NuxtLink to="/About">About</NuxtLink>
+    </li>
+    <li class="text-blue-500 hover:text-blue-800">
+      <NuxtLink to="/">example</NuxtLink>
+    </li>
+    <li class="text-blue-500 hover:text-blue-800">
+      <NuxtLink to="/">example</NuxtLink>
     </li>
   </ul>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Hellow</h1>
+  <main class="bg-gray-300 w-[940px] mx-auto h-96">
+    <h1 class="p-6 text-3xl">
+      Welcome to My Portfolio Site!<br />Frontend Engineer
+    </h1>
+  </main>
 </template>
