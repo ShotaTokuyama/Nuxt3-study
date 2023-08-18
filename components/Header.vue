cd2664ad0741e72de76c92e297a49e934282fd77
tokuyama_shota <tokush1031@gmail.com>
2023-08-18 15:34:33 +0900

    Changed NuxtLink

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="flex">
-    <li class="mr-6">
-      <a class="text-blue-500 hover:text-blue-800" href="/">Home</a>
+    <li class="mr-6 text-blue-500 hover:text-blue-800">
+      <NuxtLink to="/">Home</NuxtLink>
     </li>
-    <li class="mr-6">
-      <a class="text-blue-500 hover:text-blue-800" href="/About">About</a>
+    <li class="mr-6 text-blue-500 hover:text-blue-800">
+      <NuxtLink to="/About">About</NuxtLink>
     </li>
   </ul>
 </template>
